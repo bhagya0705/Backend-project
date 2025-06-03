a35b92a6b22842cc58f37e26750fded4461d32a7
bhagya0705 <edak.bhagya@gmail.com>
2025-06-03 13:23:16 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -8,4 +8,9 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+import userRoutes from './routes/user.routes.js';
+
+app.use('/api/v1/users',userRoutes);
+
 export default app;
