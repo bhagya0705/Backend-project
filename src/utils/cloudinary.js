8c1abbeab3a0286b66069c4a6426522c27b378b6
bhagya0705 <edak.bhagya@gmail.com>
2025-06-03 11:27:18 +0530

    added upload features

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { log } from 'console';
+import fs from 'fs';
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+const uploadonCloudinary = async (filePath) => {
+     try {
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto'
+        });
+        console.log('File uploaded successfully :', response.url);
+        
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        console.error('Error uploading to Cloudinary:', error);
+        throw error;
+    }
+}
